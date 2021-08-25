b2055105c5f71436c85d04c325533653967f7508
manis <manishbhandari916@gmail.com>
2021-08-24 20:20:59 -0500

    added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("manish");
+		System.out.println("this");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
