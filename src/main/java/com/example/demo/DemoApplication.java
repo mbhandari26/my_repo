8f576abd9bf1e0d5411701be2640ba6ac712a580
manis <manishbhandari916@gmail.com>
2021-09-26 23:32:34 -0500

    revert

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Commit1");
-		System.out.println("commit2");
-		System.out.println("Hello");
+		System.out.println("manish");
+		System.out.println("this");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
