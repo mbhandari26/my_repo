0c105015cdd1e2133fd06d2ce7858225fa09b36e
manis <manishbhandari916@gmail.com>
2021-09-26 21:38:05 -0500

    cherry picked from story

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("manish");
-		System.out.println("this");
+		System.out.println("Commit1");
+		System.out.println("commit2");
+		System.out.println("Hello");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
