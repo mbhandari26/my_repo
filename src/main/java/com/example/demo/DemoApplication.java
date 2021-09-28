f256a0aea28e6a7129cd54451ce13513dace1e4a
manis <manishbhandari916@gmail.com>
2021-09-28 16:22:37 -0500

    Revert "Merge branch 'master' into story/cust11"
    This reverts commit 78dff439d40370bb60c2392f844261b7af950e97, reversing
    changes made to aa0974560d00687bfafd400c49ee868ebc09ff12.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,9 +10,7 @@ public class DemoApplication {
 		System.out.println("Commit1");
 		System.out.println("commit2");
 		System.out.println("commit3");
-		System.out.println("revert this");
 		SpringApplication.run(DemoApplication.class, args);
-
 	}
 
 }
