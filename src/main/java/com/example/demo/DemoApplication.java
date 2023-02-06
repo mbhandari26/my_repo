d3513e85d475948412ff20258bcb97b30e69e600
manish <manishbhandari916@gmail.com>
2023-02-06 16:43:45 -0600

    merged

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("merged");
 		System.out.println("its me");
 		System.out.println("commit2");
 		System.out.println("Hello");
